f8db1cdbecd120404f2e3789e70f77d1be3285c6
Bryan Duggan <skooter500@gmail.com>
2013-11-10 12:17:06 +0000

    Initial version

---
+++ src/com/example/minbuffsizebug/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.minbuffsizebug;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	Button button;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		button = (Button) findViewById(R.id.button1);
+		button.setOnClickListener(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		AudioRecord.getMinBufferSize(44100, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+		Log.d("Test", "Got passed AudioRecord.getMinBufferSize");
+	}
+
+}
